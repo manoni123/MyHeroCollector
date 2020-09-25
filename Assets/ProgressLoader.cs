daca86f99a1175d89e65d6b27676891eae225b68
MyHeroCollector <ronas32@gmail.com>
2020-09-25 13:49:09 +0300

    added scene manager with loading

---
+++ Assets/ProgressLoader.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Security.Permissions;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ProgressLoader : MonoBehaviour
+{
+    [SerializeField]
+    private Slider slider;
+
+    private AsyncOperation opertaion;
+    public GameObject sliderObj;
+    // Start is called before the first frame update
+    void Awake()
+    {
+        DontDestroyOnLoad(gameObject);
+    }
+
+    public void LoadScene(string sceneName)
+    {
+        sliderObj.SetActive(true);
+        UpdatedProgressUI(0);
+        StartCoroutine(BeginLoad(sceneName));
+    }
+
+    private IEnumerator BeginLoad(string sceneName)
+    {
+        opertaion = Application.LoadLevelAsync(sceneName);
+
+        while (!opertaion.isDone)
+        {
+            UpdatedProgressUI(opertaion.progress);
+            yield return null;
+        }
+
+        UpdatedProgressUI(opertaion.progress);
+        opertaion = null;
+        gameObject.SetActive(false);
+    }
+
+    void UpdatedProgressUI(float progress)
+    {
+        slider.value = progress;
+    }
+}
---
+++ Assets/SceneManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class SceneManager : MonoBehaviour
+{
+    ProgressLoader _loader;
+    [SerializeField]
+    private string levelName;
+
+    private void Awake()
+    {
+        _loader = FindObjectOfType<ProgressLoader>();
+    }
+    public void LoadGame()
+    {
+        _loader.LoadScene(levelName);
+    }
+}
